6910de813c00000f56b434b024a6d6e4028eea10
hempipat <tonvongolaxi02@gmail.com>
2022-12-07 11:00:29 +0700

    add my keys config

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,10 +1,12 @@
+local opts = { noremap=true, silent=true }
+
 -- Space as leader key
 vim.g.mapleader = " "
 
 -- Shortcuts
 vim.keymap.set({"n", "x", "o"}, "<leader>h", "^")
 vim.keymap.set({"n", "x", "o"}, "<leader>l", "g_")
-vim.keymap.set("n", "<leader>a", ":keepjumps normal! ggVG<cr>")
+vim.keymap.set("n", "<leader>a", ":keepjumps normal! ggVG<cr>", opts)
 
 -- Basic cllipboard interaction
 vim.keymap.set({'n', 'x'}, 'cp', '"+y')
@@ -17,7 +19,7 @@ vim.keymap.set({'n', 'x'}, 'x', '"_x')
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
 vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
 vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
-vim.keymap.set('n', '<leader>e', '<cmd>Lexplore<cr>')
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
 
 -- Disable Arrows Keys
 vim.keymap.set('n', '<UP>', '<Nop>')
@@ -37,3 +39,12 @@ vim.keymap.set('i', '<C-c>', '<Nop>')
 -- Keys to escap Insert mode
 vim.keymap.set('i', 'jk', '<ESC>')
 vim.keymap.set('i', 'kj', '<ESC>')
+
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Bufferline
+vim.keymap.set('n', '<S-l>', ':bnext<CR>')
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>')
